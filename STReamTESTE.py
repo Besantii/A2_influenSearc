dbdab02754cb8f511d097d0ea06271ff6eedbab1
Bernardo Santinoni <141862278+Besantii@users.noreply.github.com>
2023-11-29 15:24:20 -0300

    Update STReamTESTE.py

--- STReamTESTE.py
+++ STReamTESTE.py
@@ -1,7 +1,38 @@
 import streamlit as st
 import pandas as pd
 
-# ... (seu código anterior)
+# Carregar os dados do arquivo .xlsx
+df = pd.read_excel('novo_arquivo_tiktok.xlsx')
+
+# Substituir valores nulos na coluna 'challenges'
+df['challenges'].fillna('', inplace=True)
+
+# Definir as hashtags para cada nicho
+# ... (código para definir as hashtags para cada nicho)
+
+# Criar uma nova coluna 'nicho'
+df['nicho'] = 'Outro'
+
+# Atualizar as hashtags e nichos
+for nicho, hashtags in nichos_hashtags.items():
+    df.loc[df['challenges'].apply(lambda x: any(tag in x.lower() for tag in hashtags)), 'nicho'] = nicho
+
+# Filtrar apenas as colunas relevantes
+df = df[['challenges', 'nicho']]
+
+# Dividir os dados em conjunto de treinamento e teste
+train_data, test_data, train_labels, test_labels = train_test_split(df['challenges'], df['nicho'], test_size=0.2, random_state=42)
+
+# Criar o modelo
+model = make_pipeline(CountVectorizer(), MultinomialNB())
+
+# Treinar o modelo
+model.fit(train_data, train_labels)
+
+# Avaliar a precisão do modelo nos dados de teste
+accuracy = model.score(test_data, test_labels)
+print(f'Acurácia do modelo: {accuracy:.2f}')
+
 
 # Carregar a base de dados dos influenciadores (substitua pelo seu próprio caminho e nome do arquivo)
 influenciadores_df = pd.read_csv('seu_arquivo.csv')
