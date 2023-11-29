ec0d34aff172580ae856a6ecffd9880d8010bb3d
Bernardo Santinoni <141862278+Besantii@users.noreply.github.com>
2023-11-29 15:34:15 -0300

    Update TESTEtesteTESTE.py

--- TESTEtesteTESTE.py
+++ TESTEtesteTESTE.py
@@ -2,6 +2,17 @@ import streamlit as st
 import pandas as pd
 
 
+
+
+
+
+
+
+
+
+nichos_opcoes = list(nichos_hashtags.keys())
+nicho_escolhido = st.selectbox("Escolha o nicho:", nichos_opcoes)
+
 if st.button("Mostrar Influenciadores"):
     # Filtrar influenciadores com base no nicho escolhido
     influenciadores_nicho = influenciadores_df[influenciadores_df['nicho'] == nicho_escolhido]
