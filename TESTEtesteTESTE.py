8bce4a78da5e48302bc6781646512e78725626e9
Bernardo Santinoni <141862278+Besantii@users.noreply.github.com>
2023-11-30 14:33:29 -0300

    Update TESTEtesteTESTE.py

--- TESTEtesteTESTE.py
+++ TESTEtesteTESTE.py
@@ -6,14 +6,14 @@ import pandas as pd
 st.title('InfluenSearch')
 
 # Carregar os dados do arquivo .xlsx
-df = pd.read_excel('novo_arquivo_tiktok.xlsx')
+# df = pd.read_excel('novo_arquivo_tiktok.xlsx')
 
 
 
 
 # Barra de seleção para os nichos
-nichos_opcoes = ['Música', 'Comédia', 'Viagem', 'Religião', 'Moda', 'Fitness', 'Arte']
-nicho_escolhido = st.selectbox("Escolha o nicho:", nichos_opcoes)
+# nichos_opcoes = ['Música', 'Comédia', 'Viagem', 'Religião', 'Moda', 'Fitness', 'Arte']
+# nicho_escolhido = st.selectbox("Escolha o nicho:", nichos_opcoes)
 
 # Exibir a seleção feita pelo usuário
 st.write(f"Você escolheu o nicho: {nicho_escolhido}")
