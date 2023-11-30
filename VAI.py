6a949bdf4a6c55464cfb7c07355875da752cb3d3
Bernardo Santinoni <141862278+Besantii@users.noreply.github.com>
2023-11-30 17:38:20 -0300

    Update VAI.py
    tirei os bag de cor

--- VAI.py
+++ VAI.py
@@ -18,22 +18,6 @@ def obter_autores_por_nicho_e_metrica(data, nicho_escolhido, metrica):
     subset = data[data['nicho'].str.lower() == nicho_escolhido.lower()]
     return subset.sort_values(by=metrica, ascending=False)['author'].unique()
 
-# Configuração da página com cores personalizadas
-st.set_page_config(
-    page_title="INFLUENSEARCH",
-    page_icon="✨",
-    layout="wide",
-    initial_sidebar_state="expanded",
-    background-color: #49d4b7
-    text
-)
-
-# Definindo as cores
-cor_fundo = "#050505"  # Preto
-cor_texto = "#f8f8f8"  # Branco
-cor_destaque = "#49d4b7"  # Verdin-água
-
-
 # Interface do Streamlit
 st.title('INFLUENSEARCH')
 
