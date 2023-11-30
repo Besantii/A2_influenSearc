a2fb1b00d26f354e25af0d63faf4e4f08835116a
Bernardo Santinoni <141862278+Besantii@users.noreply.github.com>
2023-11-30 17:12:57 -0300

    Update VAI.py

--- VAI.py
+++ VAI.py
@@ -3,6 +3,7 @@ import pandas as pd
 
 # Define os nichos disponíveis
 NICHOS = ['Moda', 'Religião', 'Música', 'Comédia', 'Viagem', 'Fitness', 'Arte']
+METRICAS = ['likes', 'comments', 'shares', 'plays']
 
 # Função para carregar dados do CSV
 @st.cache
@@ -12,9 +13,10 @@ def carregar_dados(filepath):
     data['nicho'] = data['nicho'].astype(str)
     return data
 
-# Função para obter autores por nicho
-def obter_autores_por_nicho(data, nicho_escolhido):
-    return data[data['nicho'].str.lower() == nicho_escolhido.lower()]['author'].unique()
+# Função para obter autores por nicho e métrica de ordenação
+def obter_autores_por_nicho_e_metrica(data, nicho_escolhido, metrica):
+    subset = data[data['nicho'].str.lower() == nicho_escolhido.lower()]
+    return subset.sort_values(by=metrica, ascending=False)['author'].unique()
 
 # Interface do Streamlit
 st.title('INFLUENSEARCH')
@@ -25,9 +27,11 @@ df = carregar_dados('tabela.csv')
 # Seleção do nicho
 nicho_selecionado = st.selectbox('Escolha um nicho', NICHOS)
 
+# Seleção da métrica de ordenação
+metrica_selecionada = st.selectbox('Escolha uma métrica de ordenação', METRICAS)
+
 # Botão para mostrar autores
 if st.button('Mostrar Influencers'):
-    autores = obter_autores_por_nicho(df, nicho_selecionado)
-    st.write(f"Autores no nicho de {nicho_selecionado}:")
+    autores = obter_autores_por_nicho_e_metrica(df, nicho_selecionado, metrica_selecionada)
+    st.write(f"Autores no nicho de {nicho_selecionado} ordenados por {metrica_selecionada}:")
     st.write(autores)
-
